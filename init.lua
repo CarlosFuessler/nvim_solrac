a9f4299822fff506e7b47b8df5ef718d18efe4a1
Carlos <181351713+CarlosFuessler@users.noreply.github.com>
2025-09-28 13:05:41 +0200

    4

--- init.lua
+++ init.lua
@@ -2,28 +2,28 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
-local opts = {} 
+local opts = {}
 
 require("lazy").setup("plugins")
 
 local builtin = require("telescope.builtin")
-vim.keymap.set('n','<C-p>',builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
-vim.keymap.set('n' , '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
+vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+vim.api.nvim_set_keymap("n", "Y", "yy", { noremap = true })
